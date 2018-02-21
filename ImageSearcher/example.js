04397f3075874ca2045175ac4ed3851b1422bd1c
Timur <newt300@mail.ru>
2018-02-21 18:56:13 +0300

    icons + gitignore

--- ImageSearcher/example.js
+++ ImageSearcher/example.js
@@ -17,18 +17,20 @@ function genericOnClickYandex(info, tab) {
   });
 }
 
-function genericOnClick() {
+function genericOnClickImageSearchMode() {
   console.log("nothing to do yet");
 }
 
 // На каких элементах буит появлятся менюшка
 var contexts = ["image","video"];
 
-var parent = chrome.contextMenus.create({"title": "ImageSearcher", "contexts": contexts});
+var parent = chrome.contextMenus.create({"title": "ImageSearcher", "contexts": ["image"]});
 var child1 = chrome.contextMenus.create(
-  {"title": "Search by Google", "parentId": parent, "onclick": genericOnClickGoogle, "contexts": contexts});
+  {"title": "Search by Google", "parentId": parent, "onclick": genericOnClickGoogle, "contexts": ["image"]});
 var child2 = chrome.contextMenus.create(
-  {"title": "Search by Yandex", "parentId": parent, "onclick": genericOnClickYandex, "contexts": contexts});
-var separator = chrome.contextMenus.create({"parentId": parent, "title": "separator","type": "separator", "contexts": contexts});
+  {"title": "Search by Yandex", "parentId": parent, "onclick": genericOnClickYandex, "contexts": ["image"]});
+var separator = chrome.contextMenus.create({"parentId": parent, "title": "separator","type": "separator", "contexts": ["image"]});
 var mod2 = chrome.contextMenus.create(
-{"title": "ImageSearch mode", "parentId": parent, "onclick": genericOnClick, "contexts": contexts});
+{"title": "ImageSearch mode", "parentId": parent, "onclick": genericOnClickImageSearchMode, "contexts": contexts});
+
+var mode = chrome.contextMenus.create({"title": "ImageSearch mode", "contexts": ["video"], "onclick": genericOnClickImageSearchMode});
